6d5a22911f7989d95030c7958a6099a4aa969637
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-09 22:41:00 +0600

    Create modframework-ui/settings.gradle.kts

---
+++ modframework-ui/settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "OrangeModLoader"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
