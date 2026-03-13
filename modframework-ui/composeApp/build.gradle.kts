7d34b98c87416ef60c34add79c87a221c438f09f
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-13 16:25:08 +0600

    Update build.gradle.kts

--- modframework-ui/composeApp/build.gradle.kts
+++ modframework-ui/composeApp/build.gradle.kts
@@ -65,7 +65,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "MangoLoader"
-            packageVersion = "1.0.0"
+            packageVersion = "0.3.0"
         }
     }
 }
