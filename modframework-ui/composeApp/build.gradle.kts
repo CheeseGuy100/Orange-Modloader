20a3bd1d981cc4625f0dde27b7975249741c31e6
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-13 16:27:43 +0600

    Update build.gradle.kts

--- modframework-ui/composeApp/build.gradle.kts
+++ modframework-ui/composeApp/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         applicationId = "com.modframework.ui"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 3
         versionName = "0.3.0-Alpha"
     }
 
