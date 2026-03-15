9ce37ff3eb0ec23f89ef8eedf875ec4eb75b1edd
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-15 11:44:10 +0600

    Update MainActivity.kt

--- modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
+++ modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.modframework.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
+lateinit var appContext: Context
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
