bb29769f19cddcb3d54e6787c52d7a28597c38e2
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-18 12:56:04 +0600

    Update PreferencesManager.kt

--- modframework-ui/composeApp/src/commonMain/kotlin/com/modframework/ui/PreferencesManager.kt
+++ modframework-ui/composeApp/src/commonMain/kotlin/com/modframework/ui/PreferencesManager.kt
@@ -3,4 +3,4 @@ package com.modframework.ui
 expect object PreferencesManager {
     fun setBoolean(key: String, value: Boolean)
     fun getBoolean(key: String, default: Boolean = false): Boolean
-}
+} 
