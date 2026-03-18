5e1cf138c8ce7eb2318411c03d51bad966862fcd
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-18 12:31:19 +0600

    Update PreferencesManager.kt

--- modframework-ui/composeApp/src/commonMain/kotlin/com/modframework/ui/PreferencesManager.kt
+++ modframework-ui/composeApp/src/commonMain/kotlin/com/modframework/ui/PreferencesManager.kt
@@ -1,13 +1,6 @@
 package com.modframework.ui
 
-object PreferencesManager {
-    private val prefs = mutableMapOf<String, Any>()
-
-    fun setBoolean(key: String, value: Boolean) {
-        prefs[key] = value
-    }
-
-    fun getBoolean(key: String, default: Boolean = false): Boolean {
-        return prefs[key] as? Boolean ?: default
-    }
+expect object PreferencesManager {
+    fun setBoolean(key: String, value: Boolean)
+    fun getBoolean(key: String, default: Boolean = false): Boolean
 }
