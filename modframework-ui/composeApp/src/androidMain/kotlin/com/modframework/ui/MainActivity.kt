d373906df749989adf8f11226a5e8f6a5fffb1eb
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-18 12:34:12 +0600

    Update MainActivity.kt

--- modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
+++ modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appContext = this
+        PreferencesManager.init(this)
         setContent {
             App()
         }
