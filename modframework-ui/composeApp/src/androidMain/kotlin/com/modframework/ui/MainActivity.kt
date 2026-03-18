245ea551bccccf77fd52275f4293c1e274a1d13c
CheeseGuy100 <cheeseguy900@gmail.com>
2026-03-18 16:35:04 +0600

    Update MainActivity.kt

--- modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
+++ modframework-ui/composeApp/src/androidMain/kotlin/com/modframework/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import coil.Coil
+import coil.ImageLoader
+
 lateinit var appContext: Context
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         appContext = this
         PreferencesManager.init(this)
+        Coil.setImageLoader(
+            ImageLoader.Builder(this).build()
+        )
         setContent {
             App()
         }
     }
-} 
+}
